b0af732e9dbdff31debeb7f40bb58f1c6d786add
lmmqxyx <1368470804@qq.com>
2025-01-16 23:59:47 +0800

    feat: implement the hysteria2 successfully

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -8,6 +8,8 @@ use transfer::bridge_quic_to_tcp;
 
 use tokio_util::codec::{Decoder, Encoder};
 
+use crate::utils::bytes_to_ascii_characters;
+
 mod transfer;
 
 /// 原生 QUIC 双向流处理：循环 accept_bi()，收到数据后转发给指定 TCP 服务器，再把服务器返回数据转给客户端
@@ -18,23 +20,16 @@ pub async fn handle_raw_bi(
     // 不断 accept_bi()，处理多条双向流
     loop {
         match conn.accept_bi().await {
-            Ok((wstream, mut rstream)) => {
+            Ok((mut wstream, mut rstream)) => {
                 info!("(raw QUIC) got a new bidirectional stream");
                 // handle_single_bstream().await;
                 let mut initial_buf = [0u8; 2];
 
                 /* let res = rstream.read_to_end(2).await?;
                 info!("res is {:?}", res); */
+                rstream.read_exact(&mut initial_buf).await.unwrap();
 
-                let n = match rstream.read(&mut initial_buf).await? {
-                    Some(n) => n, // 实际读到的字节数
-                    None => {
-                        // 客户端在还没发数据就关了写端
-                        info!("client sent no data before closing");
-                        return Ok(());
-                    }
-                };
-                info!("n is {:?}, initial is {:?}", n, initial_buf); // 为每个 QUIC bidirectional stream 启一个任务去转发
+                info!("n is , initial is {:?}", initial_buf); // 为每个 QUIC bidirectional stream 启一个任务去转发
                 if !(initial_buf[0] == 68 && initial_buf[1] == 0x01) {
                     error!("(raw QUIC) accept_bi failed: {:?}", initial_buf);
                     // 正常来说应该是返回一个错误码给客户端，这里简单处理直接关闭连接
@@ -48,18 +43,32 @@ pub async fn handle_raw_bi(
                 rstream.read_exact(&mut addr).await.unwrap();
                 info!("addr is {:?}", addr);
 
-                // addr
+                let remote_addr = bytes_to_ascii_characters(&addr);
+                info!(remote_addr);
+                let mut padd_len = BytesMut::from_iter(vec![0; 2]);
+                rstream.read_exact(&mut padd_len).await.unwrap();
+                info!("padd_len is {:?}", padd_len);
+                // VarInt::decode(&mut padd_len);
+                padd_len[0] = padd_len[0] & 0x3f;
+                let padd_len_num = u16::from_be_bytes(padd_len[0..2].try_into().unwrap());
+                info!("padd_len is {:?}", padd_len_num);
+                let mut padd_buf = BytesMut::from_iter(vec![0; padd_len_num as usize]);
+                rstream.read_exact(&mut padd_buf).await.unwrap();
+
+                wstream.write(&[0x00, 0x00, 0x00]).await.unwrap();
+
                 tokio::spawn(async move {
                     // 1) 连接到远程 TCP 服务器
-                    let remote_addr = "cp.cloudflare.com:80";
-                    match tokio::net::TcpStream::connect(remote_addr).await {
+                    // let remote_addr = "cp.cloudflare.com:80";
+                    match tokio::net::TcpStream::connect(&remote_addr).await {
                         Ok(tcp_stream) => {
+                            info!("started to send data to remote");
                             if let Err(e) = bridge_quic_to_tcp(rstream, wstream, tcp_stream).await {
                                 error!("bridge error: {}", e);
                             }
                         }
                         Err(e) => {
-                            error!("connect to {} failed: {}", remote_addr, e);
+                            error!("connect to {} failed: {}", &remote_addr, e);
                             // 如果需要，可以给客户端发回一个提示或直接关闭。
                         }
                     }
